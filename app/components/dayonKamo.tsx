a39736ee5f49e8082b3bb4dcb0aadd25e68c4809
MaxLarry <larryjohnandonga.dev@gmail.com>
2025-02-26 00:11:47 +0800

    initial commit for section photo wrap

--- app/components/dayonKamo.tsx
+++ app/components/dayonKamo.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 const dayonKamo = () => {
   return (
     <section className='section section-photo-wrapper'>
-        <div className='photo-wrapper'>
-            <div className=''>
-            {/* <Image></Image> */}
+        <div className='photo-wrapper-sticky'>
+            <div className='content-wrap photo left'>
+            {/* <Image></Image> */} papa mo
             </div>
-            <div className=''></div>
-            <div className=''>
-            {/* <Image></Image> */}
+            <div className='content-wrap '>mama mo</div>
+            <div className='content-wrap photo right'>
+            {/* <Image></Image> */} ate mo
             </div>
         </div>
     </section>
